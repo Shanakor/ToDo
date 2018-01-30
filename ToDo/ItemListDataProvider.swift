8dd16aad57243fa3bd63707b9188b98fe3ec8f79
shanakor <n.rammerstorfer@icloud.com>
2018-01-30 15:06:14 +0100

    Implemented correct number of rows inside sections.

--- ToDo/ItemListDataProvider.swift
+++ ToDo/ItemListDataProvider.swift
@@ -8,10 +8,39 @@
 
 import UIKit
 
+enum Section: Int{
+    case toDo
+    case done
+}
+
 class ItemListDataProvider: NSObject, UITableViewDataSource {
 
+    var itemManager: ItemManager?
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+
+        guard let itemManager = itemManager else{
+            return 0
+        }
+
+        guard let section = Section(rawValue: section) else{
+            fatalError()
+        }
+
+        let numberOfRows: Int
+
+        switch section{
+            case .toDo:
+                numberOfRows = itemManager.toDoCount
+            case .done:
+                numberOfRows = itemManager.doneCount
+        }
+
+        return numberOfRows
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
---
+++ ToDoTests/ItemListDataProviderTests.swift
@@ -0,0 +1,59 @@
+//
+//  ItemListDataProviderTests.swift
+//  ToDoTests
+//
+//  Created by Niklas Rammerstorfer on 30.01.18.
+//  Copyright © 2018 Niklas Rammerstorfer. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo
+
+class ItemListDataProviderTests: XCTestCase {
+
+    var sut: ItemListDataProvider!
+    var tableView: UITableView!
+
+    override func setUp() {
+        super.setUp()
+
+        sut = ItemListDataProvider()
+        sut.itemManager = ItemManager()
+        tableView = UITableView()
+        tableView.dataSource = sut
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test_NumberOfSections_IsTwo(){
+        XCTAssertEqual(tableView.numberOfSections, 2)
+    }
+
+    func test_NumberOfRows_InSection1_IsToDoCount(){
+        sut.itemManager?.add(ToDoItem(title: "foo"))
+
+        XCTAssertEqual(tableView.numberOfRows(inSection: 0), 1)
+
+        sut.itemManager?.add(ToDoItem(title: "bar"))
+        tableView.reloadData()
+
+        XCTAssertEqual(tableView.numberOfRows(inSection: 0), 2)
+    }
+
+    func test_NumberOfRow_InSection2_IsDoneCount(){
+        sut.itemManager?.add(ToDoItem(title: "foo"))
+        sut.itemManager?.add(ToDoItem(title: "bar"))
+        sut.itemManager?.checkItem(at: 0)
+
+        XCTAssertEqual(tableView.numberOfRows(inSection: 1), 1)
+
+        sut.itemManager?.checkItem(at: 0)
+        tableView.reloadData()
+
+        XCTAssertEqual(tableView.numberOfRows(inSection: 1), 2)
+    }
+    
+}
